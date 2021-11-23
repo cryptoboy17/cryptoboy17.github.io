5a8941b9f6ce9c04c546151b0bff1a2f3006266a
cryptoboy17 <smokeear@yahoo.com>
2021-11-24 00:41:34 +0700

    penambahan fitur

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const countDown = () => {
-    const countDate = new Date('Nov 22, 2021, 23:52:00').getTime();
+    const countDate = new Date('Nov 23, 2021, 20:20:00').getTime();
     const now = new Date().getTime()
     const gap = countDate - now;
-    let random = "Selamat Ulang tahun"
 
     // How the fuck does time work?
     const second = 1000;
@@ -25,7 +24,76 @@ const countDown = () => {
 
     if (gap < 1000) {
         clearInterval(time)
+        removeMain()
+        birthday()
     }
 }
 
 const time = setInterval(countDown, 1000)
+
+function removeMain() {
+    document.querySelector('main').style.opacity = 0;
+    document.querySelector('main').style.transition = '1s';
+}
+
+function birthday() {
+    const comingSoon = document.querySelector('.coming-soon');
+    const content = `<div class="greeting">
+        <h2 id=greeting></h2>
+        <h3 id=wish></h3> 
+    </div>`
+
+    setTimeout(() => {
+        comingSoon.innerHTML = content
+        greeting()
+    }, 1000)
+    setTimeout(() => {
+        wish()
+    }, 6000)
+}
+
+
+function greeting() {
+    let greeting = new TypeIt("#greeting", {
+        strings: "HAPPY BIRTHDAY LULUUU !",
+        lifeLike: true,
+        speed: 100,
+        waitUntilVisible: true,
+
+    }).go();
+}
+
+function wish() {
+    let wish = new TypeIt("#wish", {
+            speed: 50,
+            waitUntilVisible: true,
+        })
+        .type("ngomong apa yak wkwkw", {
+            delay: 600
+        })
+        .move(-10)
+        .delete(20)
+        .type("selamat ulang tasdasd")
+        .move(null, {
+            to: "END"
+        })
+        .type(" let yees")
+        .pause(300)
+        .delete(2)
+        .type("sterday use up to muc")
+        .move(-4)
+        .type("o")
+        .move(null, {
+            to: "END"
+        })
+        .type("h of today.")
+        .pause(500)
+        .break({
+            delay: 500
+        })
+        .break({
+            delay: 500
+        })
+        .type("<em>- R</em>")
+        .go();
+}
